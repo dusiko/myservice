0d99830d03f16bf4ea7eb13445b7879809747a2c
dusiko <zheka_yura@ukr.net>
2022-09-27 22:42:16 +0300

    added some js events

--- js/script.js
+++ js/script.js
@@ -12,20 +12,141 @@ function focus(event) {
 	}
 }
 
-function addAddress(event) {
+function changingValuesInList(event) {
 	if (event.target.classList.contains('service-address')) {
-		const input = document.querySelector('#modal-address-input')
+		const input = document.querySelector('#service-modal-input')
+		const title = document.getElementById('service-modal-title')
+		const text = document.querySelector('.modal-text')
+		input.setAttribute('type', 'text')
+		text.innerText = 'Адреса'
+		title.innerText = 'Адреса сервіс-центра'
 		input.value = ''
-		const address_block = event.target
+		const block = event.target
 		const btn = document.getElementById('modalOkBtn')
 		btn.onclick = () => {
 			const address = input.value
 			if (address.trim().length < 1)
-				address_block.innerText = 'Вкажіть адресу сервіс-центра'
+				block.innerText = 'Вкажіть адресу сервіс-центра'
 			else
-				address_block.innerText = address
+				block.innerText = address
 		}
+	} else if (event.target.classList.contains('service-send')) {
+		const input = document.querySelector('#service-modal-input')
+		const title = document.getElementById('service-modal-title')
+		const text = document.querySelector('.modal-text')
+		input.setAttribute('type', 'date')
+		text.innerText = 'Відправка'
+		title.innerText = 'Дата відправки на сервіс-центр'
+		input.value = ''
+		const block = event.target
+		const btn = document.getElementById('modalOkBtn')
+		btn.onclick = () => {
+			const address = input.value
+			if (address.trim().length < 1)
+				block.innerText = '--.--.--'
+			else
+				block.innerText = address.split('-').reverse().join('.')
+		}
+
+	} else if (event.target.classList.contains('service-back')) {
+		const input = document.querySelector('#service-modal-input')
+		const title = document.getElementById('service-modal-title')
+		const text = document.querySelector('.modal-text')
+		input.setAttribute('type', 'date')
+		text.innerText = 'Повернення'
+		title.innerText = 'Дата повернення з сервіс-центра'
+		input.value = ''
+		const block = event.target
+		const btn = document.getElementById('modalOkBtn')
+		btn.onclick = () => {
+			const address = input.value
+			if (address.trim().length < 1)
+				block.innerText = '--.--.--'
+			else
+				block.innerText = address.split('-').reverse().join('.')
+		}
+
 	}
 }
+
+function switchEditing() {
+	const service = document.querySelector('.service-list')
+	const btn = document.querySelector('.btn-editing-switch')
+	const btnSave = document.querySelector('.btn-editing-save')
+	if (service.classList.contains('editing')) {
+		// Режим редагування ВИМКНУЛИ
+		service.classList.remove('editing')
+		btn.innerText = 'Перейти в режим редагування'
+		btnSave.style.display = 'none'
+
+		// Вимкнув Поп-ап клік на ДатаВідправлення, Адрес, ДатаПовернення
+		document.querySelector('.service-list').removeEventListener('click', changingValuesInList)
+		const AddressSendBackitems = document.querySelectorAll('.service-send, .service-address, .service-back')
+		AddressSendBackitems.forEach(item => item.removeAttribute('data-bs-toggle', 'modal'))
+
+		// Видано покупцю блок
+		const serviceDone = document.querySelectorAll('.service-done')
+		serviceDone.forEach(item => {
+			if (item.childNodes[0].checked)
+				item.innerText = 'Так'
+			else
+				item.innerText = 'Ні'
+		})
+
+		// Блок з видаленням
+		const deleteItems = document.querySelectorAll('.service-delete-title, .service-delete')
+		console.log(deleteItems)
+		deleteItems.forEach(item => {
+			item.parentNode.removeChild(item)
+		})
+
+	} else {
+		// Режим редагування ВКЛЮЧИЛИ
+		service.classList.add('editing')
+		btn.innerText = 'Перейти в режим перегляду'
+		btnSave.style.display = 'block'
+		// Включив Поп-ап клік на ДатаВідправлення, Адрес, ДатаПовернення
+		document.querySelector('.service-list').addEventListener('click', changingValuesInList)
+		const AddressSendBackitems = document.querySelectorAll('.service-send, .service-address, .service-back')
+		AddressSendBackitems.forEach(item => item.setAttribute('data-bs-toggle', 'modal'))
+
+		// Видано покупцю блок
+		const serviceDone = document.querySelectorAll('.service-done')
+		serviceDone.forEach(item => {
+			const input = document.createElement('input')
+			input.classList.add('service-done-input')
+			input.setAttribute('type', 'checkbox')
+			input.setAttribute('name', 'client-took')
+			if (item.innerText === 'Так')
+				input.setAttribute('checked', 'checked')
+			item.innerText = ''
+			item.appendChild(input)
+		})
+
+		// Блок з видаленням
+		const parents = document.querySelectorAll('.service-list__row')
+		parents.forEach(item => {
+			const div = document.createElement('DIV')
+			div.classList.add('service-list__row-item')
+			if (item.classList.contains('title')) {
+				div.classList.add('service-list__row-item')
+				div.classList.add('title')
+				div.classList.add('service-delete-title')
+				div.innerText = 'Видалити'
+			} else {
+				div.classList.add('service-delete')
+				const btn = document.createElement('button')
+				btn.setAttribute('type', 'button')
+				btn.classList.add('btn')
+				btn.classList.add('btn-danger')
+				btn.classList.add('btn-sm')
+				btn.innerText = '-'
+				div.appendChild(btn)
+			}
+			item.appendChild(div)
+		})
+	}
+}
+
+document.querySelector('.btn-editing-switch').addEventListener('click', switchEditing)
 document.querySelector('.service-list').addEventListener('click', focus)
-document.querySelector('.service-list').addEventListener('click', addAddress)
